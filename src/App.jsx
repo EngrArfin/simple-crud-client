62a461ff485530e177d5972dfda96cf87ba84430
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-05 01:27:17 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleAddUser = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+          method: 'POST', 
+          headers: {
+            'content-type' : 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if(data.insertedId){
+            alert('Users added successfully')
+            form.reset();
+          }
+        })
+  }
+
+
+  /* fetch('http://localhost:5000/users', {
+          method: 'POST', 
+          headers: {
+            'content-type' : 'application/json'
+          },
+          body: JSON.stringify(user)
+  
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        }) */
+  
 
   return (
     <>
       <h1>Simple CUrd</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+
+      </form>
+      
     </>
   )
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h3>welcome users</h3>
+            
+        </div>
+    );
+};
+
+export default Users;
